9c595f5b39e1503c76404bdac455da12daf2666f
Ferdinand Linnenberg <fl@umh.app>
2022-09-07 11:39:05 +0200

    fix: incorrect OID check

--- cmd/oid/100_managementConsole/100_certificates/certificates_test.go
+++ cmd/oid/100_managementConsole/100_certificates/certificates_test.go
@@ -6,13 +6,13 @@ import (
 )
 
 func TestGetCaCertificateAsn10id(t *testing.T) {
-	assert.Equal(t, []int{1, 3, 6, 1, 4, 1, 59193, 100, 100}, GetCaCertificateAsn10id())
+	assert.Equal(t, []int{1, 3, 6, 1, 4, 1, 59193, 100, 100, 100}, GetCaCertificateAsn10id())
 }
 
 func TestGetTechCertificateAsn10id(t *testing.T) {
-	assert.Equal(t, []int{1, 3, 6, 1, 4, 1, 59193, 100, 200}, GetTechCertificateAsn10id())
+	assert.Equal(t, []int{1, 3, 6, 1, 4, 1, 59193, 100, 100, 200}, GetTechCertificateAsn10id())
 }
 
 func TestGetDeviceCertificateAsn10id(t *testing.T) {
-	assert.Equal(t, []int{1, 3, 6, 1, 4, 1, 59193, 100, 300}, GetDeviceCertificateAsn10id())
+	assert.Equal(t, []int{1, 3, 6, 1, 4, 1, 59193, 100, 100, 300}, GetDeviceCertificateAsn10id())
 }
